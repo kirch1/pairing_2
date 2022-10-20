165a123c80c57ceed9cab85d9f0f847342707e9f
Jason Kirchman <kirchman.jason@icloud.com>
2022-10-19 21:00:50 -0500

    Add other friend

--- friends.js
+++ friends.js
@@ -1,7 +1,10 @@
 var bestFriend = "Adam";
+var otherFriend = "Kellyn";
 
-if(bestFriend === "Eve"){
+if(bestFriend === "Adam"){
     console.log("Hello friend!");
-} else {
+}else if(otherFriend === "Kellyn"){
+    console.log("Hello other friend!");
+}else {
     console.log("Why are you the way that you are?");
 }
